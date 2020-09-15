2b637902b90989075921bb6e5b67519b8b896872
metherton <martinetherton63@gmail.com>
2020-09-15 22:45:57 +0200

    added actual files

---
+++ src/main/java/com/martinetherton/ons/OnsApplication.java
@@ -0,0 +1,13 @@
+package com.martinetherton.ons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnsApplication.class, args);
+	}
+
+}
