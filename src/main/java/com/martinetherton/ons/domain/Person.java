3d2c78e52f0b11613f09073144df61e83d1c9de7
metherton <martinetherton63@gmail.com>
2020-09-30 20:46:17 +0200

    Added test

---
+++ src/main/java/com/martinetherton/ons/domain/Person.java
@@ -0,0 +1,10 @@
+package com.martinetherton.ons.domain;
+
+public class Person {
+    public Person(String martin, String etherton) {
+    }
+
+    public String getFirstName() {
+        return null;
+    }
+}
---
+++ src/test/java/com/martinetherton/ons/domain/PersonTest.java
@@ -0,0 +1,13 @@
+package com.martinetherton.ons.domain;
+
+
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+
+    @Test
+    public void testPersonSetters() {
+        Person person = new Person("martin", "etherton");
+
+    }
+}
