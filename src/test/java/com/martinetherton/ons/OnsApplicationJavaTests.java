c2d2e65de15989a207c3df6e9c21e6d66cb924bb
metherton <martinetherton63@gmail.com>
2020-10-27 14:17:19 +0100

    removed jobs section

--- src/test/java/com/martinetherton/ons/OnsApplicationJavaTests.java
+++ src/test/java/com/martinetherton/ons/OnsApplicationJavaTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnsApplicationTests {
+class OnsApplicationJavaTests {
 
 	@Test
 	void contextLoads() {
